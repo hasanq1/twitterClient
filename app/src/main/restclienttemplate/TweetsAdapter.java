1988d79fdcfe056dd0b222ecbc2fb95ac5d999e9
hasanq1 <44348396+hasanq1@users.noreply.github.com>
2021-08-01 23:10:49 -0500

    Add files via upload

---
+++ app/src/main/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,87 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.TimeFormatter;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    public void clear(){
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<Tweet> tweetList){
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+
+        Tweet tweet = tweets.get(position);
+
+        holder.bind(tweet);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView profileImageView;
+        TextView tvBody, tvScreenName, tvTweetTime;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            profileImageView = itemView.findViewById(R.id.profileImageView);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTweetTime = itemView.findViewById(R.id.tvTweetTime);
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            Glide.with(context).load(tweet.user.profileImageUrl).into(profileImageView);
+            Date currentTime = Calendar.getInstance().getTime();
+            String formattedTime = TimeFormatter.getTimeDifference(tweet.user.created_at);
+            tvTweetTime.setText((formattedTime));
+        }
+    }
+}
